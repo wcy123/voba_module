a117b714a42f0fa2c34a51e2f3df1126d64ab084
wcy123 <wcy123@gmail.com>
2014-10-25 22:10:16 +0800

    support the name for generic function.

--- module.c
+++ module.c
@@ -282,7 +282,7 @@ static inline void module__init_lang_module()
     voba_value_t m = voba_make_symbol_table();
     voba_hash_insert(voba_modules,id,m);
     voba_make_symbol_cstr_with_value(
-        VOBA_MODULE_LANG_MATCH, m, voba_make_generic_function());
+        VOBA_MODULE_LANG_MATCH, m, voba_make_generic_function(VOBA_MODULE_LANG_MATCH));
 }
 
 static voba_value_t all_symbols = VOBA_NIL;
