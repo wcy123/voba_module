f6ed957132debfb6e0da0940949ea3bd98ea40c4
wcy123 <wcy123@gmail.com>
2015-01-25 21:20:37 +0800

    add -ldl

--- examples/ex1/build.sh
+++ examples/ex1/build.sh
@@ -1,6 +1,6 @@
 gcc -I ../../../build/include -std=c99 -fPIC -o libhello.so -shared hello.c
 gcc -I ../../../build/include -std=c99 -o ex1 ex1.c \
-    -L ../../../build/lib -lexec_once -lgc -lvoba_value -lvoba_module
+    -L ../../../build/lib -lexec_once -lgc -lvoba_value -lvoba_module -ldl
 export EXEC_ONCE_DEBUG=1
 #export VOBA_PATH=.
 export CONFIG=release
--- examples/ex2/build.sh
+++ examples/ex2/build.sh
@@ -5,7 +5,7 @@ gcc -I ../../../build/include -I. -std=c99 -fPIC -o m1/_private/libfoo.so -share
 gcc -I ../../../build/include -I. -std=c99 -fPIC -o m2/libmain.so -shared m2/main.c
 gcc -I ../../../build/include -I. -std=c99 -fPIC -o m2/_private/libfoo.so -shared m2/_private/foo.c
 gcc -I ../../../build/include -std=c99 -o ex2 ex2.c \
-    -L ../../../build/lib -lexec_once -lgc -lvoba_value -lvoba_module
+    -L ../../../build/lib -lexec_once -lgc -lvoba_value -lvoba_module -ldl
 #export EXEC_ONCE_DEBUG=1
 #export VOBA_PATH=.
 #export CONFIG=release
